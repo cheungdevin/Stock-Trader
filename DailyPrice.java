74dd3551530855341dcbafd0336c36e599dc7f20
devin-cheung <hello@devinchueung.com>
2022-08-19 23:38:49 +0100

    use java.time

--- DailyPrice.java
+++ DailyPrice.java
@@ -1,18 +1,16 @@
-import java.time.*;
-import java.util.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 
 public class DailyPrice {
 	public String stockID;
-	public Date date;
+	public LocalDate date;
 	public double close;
 	public double open;
   	   			  		  	 			 	 	
 	public DailyPrice(String[] data) throws Exception{ 	  	
 		stockID = data[0];
-		// new Date(2000, 11, 21);
-		// Date date = parse(data[1]);
-		// LocalDate date = LocalDate.parse(data[1], DateTimeFormatter.ofPattern("d-MMM-yyyy"));
+		date = LocalDate.parse(data[1], DateTimeFormatter.ofPattern("d-MMM-yyyy"));
 		close = Double.parseDouble(data[2]);
 		open = Double.parseDouble(data[3]);
 	} 	  	   			  		  	 			 	 	
--- StockTrader.java
+++ StockTrader.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.io.*;
-import java.text.SimpleDateFormat;
+
 
 public class StockTrader {
 
