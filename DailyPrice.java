0546a779ba8f9ce41cd0198e6af7edb5f59ebebd
devin-cheung <hello@devinchueung.com>
2022-08-19 22:59:39 +0100

    new version start

--- DailyPrice.java
+++ DailyPrice.java
@@ -1,13 +1,18 @@
+import java.time.*;
+import java.util.*;
+
 
 public class DailyPrice {
 	public String stockID;
-	public String date;
+	public Date date;
 	public double close;
 	public double open;
   	   			  		  	 			 	 	
 	public DailyPrice(String[] data) throws Exception{ 	  	
 		stockID = data[0];
-        date = data[1];  
+		// new Date(2000, 11, 21);
+		// Date date = parse(data[1]);
+		// LocalDate date = LocalDate.parse(data[1], DateTimeFormatter.ofPattern("d-MMM-yyyy"));
 		close = Double.parseDouble(data[2]);
 		open = Double.parseDouble(data[3]);
 	} 	  	   			  		  	 			 	 	
