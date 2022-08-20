5d3496d9eda79c3ae364fbdcd4d141a9459639fb
Devin <89694131+devin-cheung@users.noreply.github.com>
2022-08-20 01:01:36 +0100

    new version with hashmap

--- StockTrader.java
+++ StockTrader.java
@@ -1,17 +1,19 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.LocalDate;
 
 public class StockTrader {
 
-    private static ArrayList<DailyPrice> dailyPriceAL = new ArrayList<>();
+    public static HashMap<LocalDate,DailyPrice> dailyPrice = new HashMap<>();
+    private static LocalDate today;
 
     private static void initDailyPrice() throws Exception{
 
         BufferedReader br = new BufferedReader(new FileReader("daily-price.csv"));
         String strCurrentLine;
         while ((strCurrentLine = br.readLine()) != null) {
-            dailyPriceAL.add(new DailyPrice(strCurrentLine.split(",")));
+            new DailyPrice(strCurrentLine.split(","));
         }
         br.close();
     }
@@ -20,46 +22,41 @@ public class StockTrader {
 
         initDailyPrice();
 
-        for(int i=0;i<10;i++){
-            System.out.println(dailyPriceAL.get(i).date);
+        Screen.printDateInsertion();
+        LocalDate inputDate = LocalDate.parse(Screen.keyboard.nextLine());
+        System.out.println("Input Date: " + inputDate);
+
+        for(int i=0;i<3;i++){
+            if(dailyPrice.containsKey(inputDate)){
+                today = inputDate;
+                break;
+            }
+            inputDate = inputDate.plusDays(1);
         }
+        if(today==null){
+            System.out.println("Invalid input!");
+        }
+        Screen.printMainMenu(today);
 
-        // SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
-        // Screen.printDateInsertion();
-        // String dateInString = Screen.keyboard.nextLine();
-        // Date today = formatter.parse(dateInString);
+        String option = "";
 
+        while (!option.equals("Q")) {
+            option = Screen.keyboard.nextLine();
 
-        // String option = "";
-
-        // while (!option.equals("Q")) {
-        //     System.out.println("=========== Options ============");
-        //     System.out.println("1: Split string");
-        //     System.out.println("2: Remove all substring");
-        //     System.out.println("3: Shift string");
-        //     System.out.println("4: Count vowels");
-        //     System.out.println("5: Ceaser cipher");
-        //     System.out.println("6: First unique char");
-        //     System.out.println("7: Compare string");
-        //     System.out.println("8: Reverse string");
-        //     System.out.println("================================");
-        //     System.out.println("Please choose an option (type in Q if you want to quit): ");
-        //     option = reader.next();
-
-        //     switch (option){
-        //         case "1":
-        //             split();
-        //             break;
-        //         case "2":
-        //             removeSubstring();
-        //             break;
-        //         default:
-        //             if (option.equals("Q"))
-        //                 System.out.println("Goodbye!");
-        //             else
-        //                 System.out.println("Invalid Option! Please try again: ");
-        //             break;
-        //     }
-        // }
+            switch (option){
+                case "1":
+                    // split();
+                    break;
+                case "2":
+                    // removeSubstring();
+                    break;
+                default:
+                    if (option.equals("Q"))
+                        System.out.println("Goodbye!");
+                    else
+                        Screen.printInvalidMainMenuOption();
+                    break;
+            }
+        }
     }
 }
